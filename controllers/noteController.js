a75939bcb31b94e8f5378d5095e03c236799c014
Your Name <fadhishshaji@gmail.com>
2025-04-14 14:36:26 +0530

    validations

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -6,10 +6,20 @@ const notes = await Note.find().sort({position:1})
 res.json(notes)
 }
 
+const capitalizeFirstLetter = (text) => {
+  return text?.charAt(0).toUpperCase() + text?.slice(1);
+};
+
+
 export const createNotes =  async (req, res) => {
     const { title, content } = req.body;
+    if (!title?.trim() && !content?.trim()) {
+      return res.status(400).json({ message: 'Note title or content is required.' });
+    }
     const count = await Note.countDocuments();
-    const newNote = new Note({ title, content, position: count });
+    const titleFormatted = capitalizeFirstLetter(title?.trim());
+    const contentFormatted = content?.trim();
+    const newNote = new Note({ title :titleFormatted, content:contentFormatted, position: count });
     await newNote.save();
     res.status(201).json(newNote);
   }
@@ -25,7 +35,9 @@ export const createNotes =  async (req, res) => {
   export const updateNote = async (req, res) => {
     const { id } = req.params;
     const { title, content } = req.body;
-  
+    if (!title?.trim() && !content?.trim()) {
+      return res.status(400).json({ message: 'Title or content cannot be empty.' });
+    }
     try {
       const updatedNote = await Note.findByIdAndUpdate(
         id,
