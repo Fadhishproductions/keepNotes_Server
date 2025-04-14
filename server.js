88c0719ef2117d91fb361d9c3f83dbe64cac8298
Your Name <fadhishshaji@gmail.com>
2025-04-14 15:59:37 +0530

    fix: corrected cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:process.env.CLIENT_API,
     credentials:true
 }));
 app.use(express.json());
